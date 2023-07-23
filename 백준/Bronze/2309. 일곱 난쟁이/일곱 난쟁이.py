342e85be0afa67c296d3fe61c8e62a6a72ad0d67
YoujinKang <87454579+YoujinKang@users.noreply.github.com>
2023-07-23 21:34:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,11 @@
+import itertools
+
+heights_list = [int(input()) for _ in range(9)]
+
+for i in itertools.combinations(heights_list, 7):
+    if sum(i) == 100:
+        for j in sorted(i):
+            print(j)
+        break
+
+
