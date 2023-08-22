fc046d482f8a2c125d9fffb99b2cc65db9ebb4c9
YoujinKang <87454579+YoujinKang@users.noreply.github.com>
2023-08-22 09:17:02 +0900

    [Gold II] Title: 보석 도둑, Time: 1812 ms, Memory: 106728 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,24 +1,32 @@
-import sys
 import heapq
+import sys
 input = sys.stdin.readline
 
-# 보석 n개, 무게 m, 가격 v, 가방 k개, 최대무게 c
+def myPrecious(jewel, sack):
+    total = 0
+    precious = []
+    for bound in sack:
+        while jewel and jewel[0][0] <= bound:
+            heapq.heappush(precious, -heapq.heappop(jewel)[1])
 
-n, k = map(int, input().split())  # 3 2
-gems = []
+        if precious:
+            total -= heapq.heappop(precious)
+        elif not jewel:
+            break
+    return total
+
+###
+
+n, k = map(int, input().split())
+
+jewel = []
 for _ in range(n):
-    heapq.heappush(gems, list(map(int, input().split())))  # [(1 65), (5 23), (2 99)]
-bags = [int(input()) for _ in range(k)]  # [10, 2]
-bags.sort()
+    heapq.heappush(jewel, list(map(int, input().split()))) # w, v
 
-value = 0
-temp = []
-for bag in bags:
-    while gems and gems[0][0] <= bag:  # 가장 가벼운 보석부터 현재 가방에 들어갈 수 있는 보석만 temp 힙에 넣음
-        m, v = heapq.heappop(gems)
-        heapq.heappush(temp, -v)
 
-    if temp:  # temp 힙에서 가장 큰 값을 value에 넣음 bag의 수 만큼만 돌아감
-        value += -heapq.heappop(temp)
+sack = []
+for _ in range(k):
+    sack.append(int(input()))
+sack.sort()
 
-print(value)
+print(myPrecious(jewel, sack))
