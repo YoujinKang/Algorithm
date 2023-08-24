0382f18b9eb23c6b6c36033d2ae42deed4f0da6c
YoujinKang <87454579+YoujinKang@users.noreply.github.com>
2023-08-24 11:30:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -6,13 +6,12 @@ d = [-1] * 5001
 d[3], d[5] = 1, 1
 
 for i in range(6, 5001):
-    if d[i-3] > 0 and d[i-5] > 0:
-        d[i] = min(d[i-3], d[i-5]) + 1
-    elif d[i - 3] > 0 > d[i - 5]:
-        d[i] = d[i-3] + 1
-    elif d[i - 5] > 0 > d[i - 3]:
-        d[i] = d[i - 5] + 1
-    else:
-        d[i] = -1
+    if d[i - 3] > 0:
+        d[i] = d[i - 3] + 1
+    if d[i - 5] > 0:
+        if d[i] == -1:
+            d[i] = d[i - 5] + 1
+        else:  # d[i-3] 이 먼저 돌아간 경우
+            d[i] = min(d[i], d[i-5] + 1)
 
 print(d[n])
