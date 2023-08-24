db4ae29fae1f0a4085f577661684be5e10771e70
YoujinKang <87454579+YoujinKang@users.noreply.github.com>
2023-08-24 11:34:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -2,16 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-d = [-1] * 5001
+d = [float('inf')] * 5001
 d[3], d[5] = 1, 1
 
 for i in range(6, 5001):
-    if d[i - 3] > 0:
-        d[i] = d[i - 3] + 1
-    if d[i - 5] > 0:
-        if d[i] == -1:
-            d[i] = d[i - 5] + 1
-        else:  # d[i-3] 이 먼저 돌아간 경우
-            d[i] = min(d[i], d[i-5] + 1)
+    d[i] = min(d[i - 3], d[i - 5]) + 1
 
-print(d[n])
+if d[n] == float('inf'):
+    print(-1)
+else:
+    print(d[n])
